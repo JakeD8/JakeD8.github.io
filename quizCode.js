8e3ab7c4b0a249448a4d8ab679ee153f9abd2c85
JakeD8 <159396734+JakeD8@users.noreply.github.com>
2024-02-08 22:56:55 -0500

    quizCode.js

--- quizCode.js
+++ quizCode.js
@@ -2,47 +2,69 @@ const quizData = [
     {
         question: "Where was our first date?",
         options: ["The Graveyard","McDonald's","The Hippie Store","My Dorm Room"],
-        answer: "The Graveyard"
+        answer: "The Graveyard",
+        image1: "./car.jpg",
+        image2: "./kiss.jpg"
     },{
         question: "What is our favorite Naethan Apollo Song?",
         options: ["Cannibal","Climate Crisis Love Song","To You, From Me","Dollar Store Date"],
-        answer: "To You, From Me"
+        answer1: "To You, From Me",
+        image1: "./dorm.jpg",
+        image2: "./graveface.jpg"
     },{
         question: "How many days have we dated?",
         options: ["23","173","201","159"],
-        answer: "159"
+        answer: "159",
+        image1: "./panda.jpg",
+        image2: "./parking.jpg"
     },{
         question: "How many times have we visited the ER together?",
         options: ["2","3","1.5","4"],
-        answer: "3"
+        answer: "3",
+        image1: "./park.jpg",
+        image2: "./park2.jpg"
     },{
         question: "What is my favorite color?",
         options: ["Midnight Black","Wine Red","Chartreuse","Baby Blue"],
-        answer: "Wine Red"
+        answer: "Wine Red",
+        image1: "./party.jpg",
+        image2: "./fort.jpg"
     },{
         question: "What was the first gift I ever gave you?",
         options: ["T-Shirt","Squishmallow","Stickers","Blanket"],
-        answer: "T-Shirt"
+        answer: "T-Shirt",
+        image1: "./falling.jpg",
+        image2: "./sushi.jpg"
     },{
         question: "What day will I propose to you?",
         options: ["9/24/25","4/12/26","7/29/28","9/16/26"],
-        answer: "9/16/26"
+        answer: "9/16/26",
+        image1: "./car2.jpg",
+        image2: "./hall3.jpg"
     },{
         question: "How many notes do I have from you on my wall? (including cards, letters, drawings)",
         options: ["21", "17","16","13"],
-        answer: "16"
+        answer: "16",
+        image1: "./hall1.jpg",
+        image2: "./hall4.jpg"
     },{
         question: "Solve: Cubed Root of(69/3). Round to the nearest tenth.",
         options: ["1.6","4.6","0.02","2.8"],
-        answer: "2.8"
+        answer: "2.8",
+        image1: "./tattoo.jpg",
+        image2: "./grave.jpg"
     },{
         question: "Do you love me?",
         options: ["Yes","Absolutely"],
-        answer: "Absolutely"
+        answer: "Absolutely",
+        image1: "./hall2.jpg",
+        image2: "./falling2.jpg"
     },{
         question: "Will you be my Valentine?",
         options: ["Yes","No"],
-        answer: "Yes"
+        answer: "Yes",
+        image1: "./witch.jpg",
+        image2: "./concert.jpg"
     },
 ];
 
@@ -51,6 +73,8 @@ const optionsBox = document.getElementById('options');
 const dabloonCount = document.getElementById('dabloonCount');
 const questionCount = document.getElementById('questionCount');
 const loadQuiz = document.getElementById('startQuiz');
+const picRight = document.getElementById('rightImage');
+const picLeft = document.getElementById('leftImage');
 
 let questionIndex = 0;
 let dabloons = 0;
@@ -60,6 +84,8 @@ function loadQuestion(){
     const currentQuestion = quizData[questionIndex];
     questionBox.innerText = currentQuestion.question;
     optionsBox.innerHTML = '';
+    picLeft.src=currentQuestion.image1;
+    picRight.src=currentQuestion.image2;
     currentQuestion.options.forEach((option, index) =>{
         const optionElement = document.createElement('button');
         const breakElement = document.createElement('br');
@@ -68,6 +94,8 @@ function loadQuestion(){
         optionElement.addEventListener('click', () => checkAnswer(option));
         optionsBox.appendChild(optionElement);
         optionsBox.appendChild(breakElement);
+
+
 
     });   
 }
@@ -88,7 +116,7 @@ function checkAnswer(selectedOption){
 }
 
 function startQuiz(){
-    loadQuiz.remove();
+    loadQuiz.style.display = "none";
     loadQuestion();
 }
 
